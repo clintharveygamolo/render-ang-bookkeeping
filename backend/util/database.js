e47e1669acc461a31089b1861d141aad8b99c8a0
clintharveygamolo <159156893+clintharveygamolo@users.noreply.github.com>
2024-05-26 12:50:24 +0800

    Update database.js

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,5 +1,11 @@
 import { Sequelize } from "sequelize";
 
+console.log("Connecting to database with the following details:");
+console.log("Database:", process.env.DATABASE_DB);
+console.log("User:", process.env.DATABASE_USER);
+console.log("Host:", process.env.DATABASE_HOST);
+console.log("Port:", process.env.DATABASE_PORT || 3306);
+
 const sequelize = new Sequelize(
   process.env.DATABASE_DB,
   process.env.DATABASE_USER,
