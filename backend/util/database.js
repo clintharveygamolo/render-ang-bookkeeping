0f1356bf8e71ff49f9a1fceb62311926f4fa2d7a
clintharveygamolo <159156893+clintharveygamolo@users.noreply.github.com>
2024-05-26 12:06:58 +0800

    Update database.js

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,7 +1,6 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_HOST,
   process.env.DATABASE_DB,
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD,
